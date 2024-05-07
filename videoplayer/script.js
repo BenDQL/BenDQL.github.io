e40f320f3f2df1588a486b06661f58a1b5df2157
BenDQL <131518649+BenDQL@users.noreply.github.com>
2024-05-07 20:46:52 +1000

    add week7

---
+++ videoplayer/script.js
@@ -0,0 +1,5 @@
+const myVideo = document.querySelector("#my-video");
+const playPauseButton = document.querySelector("#play-pause-button");
+const playPauseImg = document.querySelector("#play-pause-img");
+
+playPauseButton.addEventListener("click", playPauseVided);
